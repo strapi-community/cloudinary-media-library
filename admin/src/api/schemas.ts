96323ac55da4c306930d34b4146c537d7f38dfd9
matuesz-kleszcz <mkleszcz@virtuslab.com>
2025-04-30 15:35:59 +0200

    fix: fix schemas

--- admin/src/api/schemas.ts
+++ admin/src/api/schemas.ts
@@ -1,8 +1,8 @@
 import * as z from 'zod';
 
 export const configSchema = z.object({
-  cloud_name: z.string(),
-  api_key: z.string(),
+  cloud_name: z.string().optional(),
+  api_key: z.string().optional(),
 });
 
 export type Config = z.infer<typeof configSchema>;
